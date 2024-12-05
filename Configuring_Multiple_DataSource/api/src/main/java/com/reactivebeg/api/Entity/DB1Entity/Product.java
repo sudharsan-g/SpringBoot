487d163bc38618a96197bad87f05a68b0a8065a9
sudharsan-g <sudharsanganesan004@gmail.com>
2024-12-05 22:21:40 +0530

    First Commit

---
+++ Configuring_Multiple_DataSource/api/src/main/java/com/reactivebeg/api/Entity/DB1Entity/Product.java
@@ -0,0 +1,52 @@
+package com.reactivebeg.api.Entity.DB1Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Integer id;
+    private String name;
+    private Integer price;
+
+    public Product(Integer id, String name, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+}
